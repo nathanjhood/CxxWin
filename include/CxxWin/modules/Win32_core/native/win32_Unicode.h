7ed059de14f8c8a0deb1279375b1d247c5081a6d
StoneyDSP <nathanjhood@googlemail.com>
2023-08-31 22:39:28 +0200

    Added test string to Unicode handler

--- include/CxxWin/modules/Win32_core/native/win32_Unicode.h
+++ include/CxxWin/modules/Win32_core/native/win32_Unicode.h
@@ -9,11 +9,11 @@
  *
  */
 
-#if !defined(_INC_CRT_UNICODE_MACROS)
-/* _INC_CRT_UNICODE_MACROS defined based on UNICODE flag */
+#if !defined(_NEED_UNICODE)
+/* _NEED_UNICODE defined based on UNICODE flag */
 
 #if defined(UNICODE)
-# define _INC_CRT_UNICODE_MACROS 1
+# define _NEED_UNICODE 1
 # define __NAME_AW(func) func##W
 # define __NAME_AW_EXT(func,ext) func##W##ext
 # define __NAME_UAW(func) func##_W
@@ -21,7 +21,7 @@
 # define __STRING_AW(str) L##str	/* same as TEXT() from winnt.h */
 # define __PROCNAMEEXT_AW "W"
 #else
-# define _INC_CRT_UNICODE_MACROS 2
+# define _NEED_UNICODE 2
 # define __NAME_AW(func) func##A
 # define __NAME_AW_EXT(func,ext) func##A##ext
 # define __NAME_UAW(func) func##_A
@@ -36,3 +36,34 @@
     typedef __NAME_UAW(type) type;
 
 #endif /* !defined(_INC_CRT_UNICODE_MACROS) */
+
+/** The generic function prototype uses the data type LPCTSTR. */
+WINUSERAPI LPCTSTR WINAPI SetTst(LPCTSTR lpString) { return lpString; };
+
+auto unicodeTstA = SetTst(TEXT("Calling original function"));
+
+/**
+ * The header file provides the generic function name implemented as a macro.
+ * The preprocessor expands the macro into either the Windows code page or
+ * Unicode function name. The letter "A" (ANSI) or "W" (Unicode) is added at
+ * the end of the generic function name, as appropriate. The header file then
+ * provides two specific prototypes, one for Windows code pages and one for
+ * Unicode.
+ *
+ * #if defined(UNICODE)
+ * #define SetTst SetTstW
+ * #else
+ * #define SetTst SetTstA
+ * #endif
+ *
+ * @link https://learn.microsoft.com/en-us/windows/win32/intl/conventions-for-function-prototypes
+ *
+ */
+#define SetTst __NAME_AW(SetTst)
+
+/** The Windows code page prototype uses the data type LPCSTR. */
+WINUSERAPI LPCSTR WINAPI SetTstA(LPCSTR lpString) { return lpString; };
+/** The Unicode prototype uses the data type LPCWSTR. */
+WINUSERAPI LPCWSTR WINAPI SetTstW(LPCWSTR lpString) { return lpString; };
+
+auto unicodeTstB = SetTst(TEXT("Calling corrected function"));
