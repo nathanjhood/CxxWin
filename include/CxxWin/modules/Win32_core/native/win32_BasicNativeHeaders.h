c3226f0d5df4d711ea6c3e55e3d75457c4343f2d
StoneyDSP <nathanjhood@googlemail.com>
2023-08-31 22:25:14 +0200

    Added platform and architecture macros

--- include/CxxWin/modules/Win32_core/native/win32_BasicNativeHeaders.h
+++ include/CxxWin/modules/Win32_core/native/win32_BasicNativeHeaders.h
@@ -11,78 +11,6 @@
 
 #pragma once
 
-/**
- * Platform check macros
- *
- */
-
-#if defined(__MSYS__) || defined(MSYS)
-#  define __PLATFORM_IS_MSYS 1
-#endif
-
-#if defined(__CYGWIN__) || defined (CYGWIN)
-#  define __PLATFORM_IS_CYGWIN 1
-#endif
-
-#if defined (__MINGW64__) || defined(__MINGW32__) || defined (MINGW)
-#  define __PLATFORM_IS_MINGW 1
-#endif
-
-#if defined(_WIN32) || defined(__WIN32__) || defined (WIN32)
-#  define __PLATFORM_IS_WINDOWS 1
-#endif
-
-#if  defined (__linux__) || defined (__linux) || defined (linux)
-#  define __PLATFORM_IS_LINUX 1
-#endif
-
-#if defined (__APPLE__)
-#  define __PLATFORM_IS_DARWIN 1
-#endif
-
-#if defined (__FreeBSD__) || defined (__FreeBSD)
-#  define __PLATFORM_IS_FREEBSD 1
-#endif
-
-#if defined (__NetBSD__) || defined (__NetBSD)
-#  define __PLATFORM_IS_NETBSD 1
-#endif
-
-#if defined (__OpenBSD__) || defined (__OPENBSD)
-#  define __PLATFORM_IS_OPENBSD 1
-#endif
-
-#if  defined (__unix__) || defined (__unix) || defined (unix)
-#  define __PLATFORM_IS_UNIX 1
-#endif
-
-#ifndef _XOPEN_UNIX
-#  if (__PLATFORM_IS_UNIX) && defined (_XOPEN_SOURCE)
-#    define _XOPEN_UNIX 1
-#  endif
-#endif
-
-#ifdef _XOPEN_VERSION
-#  if (_XOPEN_VERSION >= 3)
-#    define __STANDARD_XOPEN_1989 1
-#  endif
-#  if (_XOPEN_VERSION >= 4)
-#    define __STANDARD_XOPEN_1992 1
-#  endif
-#  if (_XOPEN_VERSION >= 4) && defined(_XOPEN_UNIX)
-#    define __STANDARD_XOPEN_1995
-#  endif
-#  if (_XOPEN_VERSION >= 500)
-#    define __STANDARD_XOPEN_1998 1
-#  endif
-#  if (_XOPEN_VERSION >= 600)
-#    define __STANDARD_XOPEN_2003 1
-#  endif
-#  if (_XOPEN_VERSION >= 700)
-#    define __STANDARD_XOPEN_2008 1
-#  endif
-#endif
-
 #undef T
 
 //==============================================================================
@@ -183,6 +111,7 @@
  #include <winioctl.h>
 
  #include <windowsx.h>
+ #include <ole2.h>
  #include <d2d1.h>
 
  #if defined(__PLATFORM_IS_MINGW)
